96d1cc1b8d355035b9726edbfe24d3efa97b6bde
hashem333 <75488511+hashem333@users.noreply.github.com>
2023-05-13 22:39:57 +0300

    Update script.groovy - change Dockerhub to Nexus Docker Repo

--- script.groovy
+++ script.groovy
@@ -5,15 +5,15 @@ def buildDockerImage() {
 	echo "building the docker image..."
 	withCredentials([usernamePassword(credentialsId: 'my-dockerhub-repo', usernameVariable: 'USER', passwordVariable: 'PASS')])
 	{
-		sh "docker build -t hashem333/my-docker-repo:project7-new-1.0 ."
+		sh "docker build -t 172.18.0.2:8085/my-docker-repo/project7-new:1.0 ."
 		sh "echo $PASS | docker login -u $USER --password-stdin"
-		sh "docker push hashem333/my-docker-repo:project7-new-1.0"
-		sh "docker rmi hashem333/my-docker-repo:project7-new-1.0 --force" 
+		sh "docker push 172.18.0.2:8085/my-docker-repo/project7-new:1.0"
+		sh "docker rmi 172.18.0.2:8085/my-docker-repo/project7-new:1.0 --force" 
 		}
 }
 def deployDockerImage() {
-	sh "ssh hashem@172.17.0.1 docker pull hashem333/my-docker-repo:project7-new-1.0"
-	sh "ssh hashem@172.17.0.1 docker run --rm -dt -p 8088:8088 --name project7 hashem333/my-docker-repo:project7-new-1.0"
+	sh "ssh hashem@172.17.0.1 docker pull 172.18.0.2:8085/my-docker-repo/project7-new:1.0"
+	sh "ssh hashem@172.17.0.1 docker run --rm -dt -p 8088:8088 --name project7 172.18.0.2:8085/my-docker-repo/project7-new:1.0"
 }
 
 return this
